9dd0120a390850c9bc4904554c26d54a0261a141
Siddhant Agarwal <77344122+siddhant20247@users.noreply.github.com>
2021-12-06 16:07:02 +0530

    added main menu and load game menu

---
+++ src/main/java/com/example/will_hero/LoadGameMenuController.java
@@ -0,0 +1,10 @@
+package com.example.will_hero;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LoadGameMenuController {
+    @FXML
+    private Label myLabel;
+
+}
---
+++ src/main/java/com/example/will_hero/MainMenuController.java
@@ -0,0 +1,39 @@
+package com.example.will_hero;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainMenuController {
+    @FXML private AnchorPane scenePane;
+    @FXML private Button newGameButton;
+    @FXML private Button loadGameButton;
+    @FXML private Button exitGameButton;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void exitGame(ActionEvent event) {
+        stage = (Stage) scenePane.getScene().getWindow();
+        stage.close();
+    }
+
+    public void loadGame(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("load-game-menu.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
